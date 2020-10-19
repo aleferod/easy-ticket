a35eedc06d99bd1905ff315582048c82bdd56807
Alefe Oliveira <alefeoliveira.verity@azulseguros.com.br>
2020-10-19 11:34:24 -0300

    Initialize repository

---
+++ src/main/kotlin/br/com/example/easyticket/EasyTicketApplication.kt
@@ -0,0 +1,11 @@
+package br.com.example.easyticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasyTicketApplication
+
+fun main(args: Array<String>) {
+	runApplication<EasyTicketApplication>(*args)
+}
