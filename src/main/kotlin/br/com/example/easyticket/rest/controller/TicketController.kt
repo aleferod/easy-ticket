baaebd2a8389d47613f4efba186725e125e2c53e
Alefe Oliveira <alefeoliveira.verity@azulseguros.com.br>
2020-10-19 21:00:56 -0300

    Corrigindo annotation

--- src/main/kotlin/br/com/example/easyticket/rest/controller/TicketController.kt
+++ src/main/kotlin/br/com/example/easyticket/rest/controller/TicketController.kt
@@ -5,7 +5,7 @@ import br.com.example.easyticket.entity.Ticket
 import br.com.example.easyticket.rest.service.TicketService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import javax.websocket.server.PathParam
+
 
 @RestController
 @RequestMapping("/ticket")
@@ -18,12 +18,12 @@ class TicketController (val service: TicketService){
     fun findAll() = ResponseEntity.ok(service.findAll())
 
     @GetMapping("/document")
-    fun findByDocument(@PathParam("documento") document: String) = ResponseEntity.ok(service.findByDocument(document))
+    fun findByDocument(@PathVariable("documento") document: String) = ResponseEntity.ok(service.findByDocument(document))
 
     @PutMapping
     fun update(@RequestBody ticketDto: TicketDto) = ResponseEntity.ok(service.save(ticketDto))
 
     @DeleteMapping
-    fun remove(@PathParam("id") id: String) = ResponseEntity.ok(service.remove(id))
+    fun remove(@PathVariable("id") id: String) = ResponseEntity.ok(service.remove(id))
 
 }
