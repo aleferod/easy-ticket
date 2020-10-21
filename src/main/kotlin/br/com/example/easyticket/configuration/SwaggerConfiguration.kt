f6b359040438987c804714c4d0462f689ff94403
Alefe Oliveira <alefeoliveira.verity@azulseguros.com.br>
2020-10-21 16:43:23 -0300

    Melhorando a Info da documentação

--- src/main/kotlin/br/com/example/easyticket/configuration/SwaggerConfiguration.kt
+++ src/main/kotlin/br/com/example/easyticket/configuration/SwaggerConfiguration.kt
@@ -2,8 +2,10 @@ package br.com.example.easyticket.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -20,5 +22,7 @@ class SwaggerConfiguration {
                 .apis(RequestHandlerSelectors.basePackage("br.com.example.easyticket.rest.controller"))
                 .build()
     }
+
+    fun apiInfo(): ApiInfo = ApiInfoBuilder().title("Easy Ticket API").description("Documentação da API Easy Ticket").build();
 
 }
