5b6dcdef1f47efe62df1c18b0245508ceb4e06fd
Alefe Oliveira <alefeoliveira.verity@azulseguros.com.br>
2020-10-21 15:22:15 -0300

    Script para instalação e start do mongo-db via docker

---
+++ mongo-dockerized.sh
@@ -0,0 +1,8 @@
+docker pull tutum/mongodb
+
+docker stop mongo-db
+docker rm mongo-db
+
+docker run -d -p 27017:27017 -p 28017:28017 --name=mongo-db -e AUTH=no tutum/mongodb
+
+winpty docker exec -it mongo-db bash
